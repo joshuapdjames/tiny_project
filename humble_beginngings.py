8d18751c2837ebcf0bd11e2b9328aa3f424bf402
datasandwich <72459044+datasandwich@users.noreply.github.com>
2021-02-10 13:02:18 +0000

    Add files via upload

---
+++ humble_beginngings.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 10 11:04:18 2021
+
+@author: datasandwich
+"""
+import pandas as pd
+from scipy.io import mmread
+
+matrix=mmread('/Users/datasandwich/Documents/Mini Project/filtered_gene_bc_matrices/hg19/matrix.mtx')
+B=matrix.todense()
+matrix_df=pd.DataFrame(B)
+
+barcodes=pd.read_csv('/Users/datasandwich/Documents/Mini Project/filtered_gene_bc_matrices/hg19/barcodes.tsv')
+genes=pd.read_csv('/Users/datasandwich/Documents/Mini Project/filtered_gene_bc_matrices/hg19/genes.tsv')
+sng_cll_mrkers=pd.read_table('/Users/datasandwich/Documents/Mini Project/Single_cell_markers.txt')
+hmn_cll_mrkrs=pd.read_table('/Users/datasandwich/Documents/Mini Project/human_cell_markers.txt')
