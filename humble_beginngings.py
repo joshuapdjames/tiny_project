4eb0f38b7307f258d40cd5bd88ea166d34af0d72
datasandwich <72459044+datasandwich@users.noreply.github.com>
2021-02-10 15:34:51 +0000

    Update humble_beginngings.py

--- humble_beginngings.py
+++ humble_beginngings.py
@@ -8,16 +8,15 @@ Created on Wed Feb 10 11:04:18 2021
 import pandas as pd
 from scipy.io import mmread
 
-matrix=mmread('/Users/datasandwich/Documents/Mini Project/filtered_gene_bc_matrices/hg19/matrix.mtx')
+matrix=mmread('filtered_gene_bc_matrices/hg19/matrix.mtx')
 B=matrix.todense()
 matrix_df=pd.DataFrame(B)
 
-ecurd=mmread('/Users/datasandwich/Documents/Mini Project/bristoluni/zhengdata/E-CURD-55-normalised-files/E-CURD-55.aggregated_filtered_normalised_counts.mtx')
+ecurd=mmread('zhengdata/E-CURD-55-normalised-files/E-CURD-55.aggregated_filtered_normalised_counts.mtx')
 A=matrix.todense()
 ecurd_df=pd.DataFrame(A)
 
-
-barcodes=pd.read_csv('/Users/datasandwich/Documents/Mini Project/filtered_gene_bc_matrices/hg19/barcodes.tsv')
-genes=pd.read_csv('/Users/datasandwich/Documents/Mini Project/filtered_gene_bc_matrices/hg19/genes.tsv')
-sng_cll_mrkers=pd.read_table('/Users/datasandwich/Documents/Mini Project/Single_cell_markers.txt')
-hmn_cll_mrkrs=pd.read_table('/Users/datasandwich/Documents/Mini Project/human_cell_markers.txt')
+barcodes=pd.read_csv('filtered_gene_bc_matrices/hg19/barcodes.tsv')
+genes=pd.read_csv('filtered_gene_bc_matrices/hg19/genes.tsv')
+sng_cll_mrkers=pd.read_table('Single_cell_markers.txt')
+hmn_cll_mrkrs=pd.read_table('human_cell_markers.txt')
