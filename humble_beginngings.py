5b0064224d56caf8b931b85b251d126c2bd8540a
datasandwich <72459044+datasandwich@users.noreply.github.com>
2021-02-10 13:39:00 +0000

    Update humble_beginngings.py

--- humble_beginngings.py
+++ humble_beginngings.py
@@ -12,6 +12,11 @@ matrix=mmread('/Users/datasandwich/Documents/Mini Project/filtered_gene_bc_matri
 B=matrix.todense()
 matrix_df=pd.DataFrame(B)
 
+ecurd=mmread('/Users/datasandwich/Documents/Mini Project/bristoluni/zhengdata/E-CURD-55-normalised-files/E-CURD-55.aggregated_filtered_normalised_counts.mtx')
+A=matrix.todense()
+ecurd_df=pd.DataFrame(A)
+
+
 barcodes=pd.read_csv('/Users/datasandwich/Documents/Mini Project/filtered_gene_bc_matrices/hg19/barcodes.tsv')
 genes=pd.read_csv('/Users/datasandwich/Documents/Mini Project/filtered_gene_bc_matrices/hg19/genes.tsv')
 sng_cll_mrkers=pd.read_table('/Users/datasandwich/Documents/Mini Project/Single_cell_markers.txt')
